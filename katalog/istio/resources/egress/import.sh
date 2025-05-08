ee92311f0dfcc0d7ed3ae8499a045052a59ae963
Stefano Ghinelli <stefano.ghinelli@sighup.io>
2025-05-08 12:46:58 +0200

    chore: add license header

--- katalog/istio/resources/egress/import.sh
+++ katalog/istio/resources/egress/import.sh
@@ -1,3 +1,7 @@
+# Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 #!/bin/bash -e
 # refresh.sh
 # Create a kustomization.yaml and then add all kubernetes YAMLs into resources...
