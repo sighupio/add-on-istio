0a9956f555fbf0e355f2dd395bcaaad98d4fb9b8
Stefano Ghinelli <stefano.ghinelli@sighup.io>
2025-05-08 15:04:44 +0200

    fix: temporary disable SC1128,SC2068

--- katalog/istio/resources/egress/import.sh
+++ katalog/istio/resources/egress/import.sh
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-shellcheck disable=SC1128,SC2068
+# shellcheck disable=SC1128,SC2068
 
 
 # refresh.sh
